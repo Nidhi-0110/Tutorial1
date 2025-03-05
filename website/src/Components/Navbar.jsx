3edbefbc36d5c2dd0c462aaef50b34174dc6408d
Nidhi-0110 <patelnidhi1628@gmail.com>
2025-03-05 12:10:04 +0530

    change and update

--- website/src/Components/Navbar.jsx
+++ website/src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <>
@@ -10,10 +11,13 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/about">About</a>
+                                <Link className="nav-link" to="/about">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/services">Services</Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
